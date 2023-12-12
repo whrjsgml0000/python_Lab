2e88adbc3fc067cbbc6f973f3d7138d76185c2a1
whrjsgml0000 <153486036+whrjsgml0000@users.noreply.github.com>
2023-12-12 22:42:02 +0900

    filter 사용

--- hello.py
+++ hello.py
@@ -1,10 +1,5 @@
-try:
-    f = open("none","r")
-except FileNotFoundError as e:
-    print(e)
-else:
-    data = f.read()
-    print(data)
-    f.close()
-finally:
-    print("프로그램을 종료합니다.")
+def positive(x):
+    return x > 0
+
+a = list(filter(positive, [1, -3, 2, 0, -5, 6]))
+print(a)
